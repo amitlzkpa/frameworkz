6f5b220493b6f7dcb825803b82f40b486e6b02d7
amitlzkpa <amit.lzkpa@gmail.com>
2021-12-04 20:05:51 -0800

    make about pagie using template; working proper

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
+async function onBtnClick_Landing() {
+  let temp_MainCont = document.getElementById("content-landing");
+  let node_MainCont = temp_MainCont.content.cloneNode(true);
+  let targetNode = document.getElementsByTagName("main")[0];
+  targetNode.innerHTML = "";
+  targetNode.appendChild(node_MainCont);
+}
+
 async function onBtnClick_Abt() {
-  let aboutDiv = document.getElementById("about-div");
-  aboutDiv.classList.toggle("fadeIn");
-  aboutDiv.classList.toggle("fadeOut");
+  let temp_AbtCont = document.getElementById("content-about");
+  let node_AbtCont = temp_AbtCont.content.cloneNode(true);
+  let targetNode = document.getElementsByTagName("main")[0];
+  targetNode.innerHTML = "";
+  targetNode.appendChild(node_AbtCont);
 }
 
 async function main() {
-  let btnAbt = document.getElementById("btn-about");
-  btnAbt.addEventListener(
-    "click",
-    onBtnClick_Abt,
-    false
-  );
+  onBtnClick_Landing();
+
+  document
+    .getElementById("navbar-logo")
+    .addEventListener("click", onBtnClick_Landing, false);
+
+  document
+    .getElementById("btn-about")
+    .addEventListener("click", onBtnClick_Abt, false);
 }
 
 window.addEventListener("load", (event) => {
