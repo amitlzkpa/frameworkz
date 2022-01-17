ed8ff8a111e8b0e3edca0293e89c6e274d975fc3
Amit Nambiar <15354742+amitlzkpa@users.noreply.github.com>
2022-01-17 00:30:37 -0800

    add a decently functional hamburger nav menu

--- js/index.js
+++ js/index.js
@@ -15,7 +15,10 @@ async function onBtnClick_Abt() {
 }
 
 async function onBtnClick_Hamburger() {
-  console.log("foo");
+  let menu_Modal = document.querySelector("#navbar-list-menu-modal");
+  let isInView =
+    !!menu_Modal.style.visibility && menu_Modal.style.visibility !== "hidden";
+  menu_Modal.style.visibility = isInView ? "hidden" : "visible";
 }
 
 async function main() {
@@ -27,12 +30,12 @@ async function main() {
     .addEventListener("click", onBtnClick_Landing, false);
 
   document
-    .querySelector("#navbar-list-menu-anchor")
+    .querySelector("#navbar-list-menu-container")
     .addEventListener("click", onBtnClick_Hamburger, false);
 
   document
-    .querySelector(".btn-about")
-    .addEventListener("click", onBtnClick_Abt, false);
+    .querySelectorAll(".btn-about")
+    .forEach((btn) => btn.addEventListener("click", onBtnClick_Abt, false));
 }
 
 window.addEventListener("load", (event) => {
